0d911c36ad3f88b1ac2afbfbfd4aa2f084d59eec
sekkat-rajae <rajaeamal301@gmail.com>
2026-04-21 19:37:03 +0100

    Initial commit: backend + frontend

---
+++ Kids-IT-Learning-Platform/src/main/java/com/rajae_sekkat/Kids_IT_Learning_Platform/KidsItLearningPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.rajae_sekkat.Kids_IT_Learning_Platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidsItLearningPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidsItLearningPlatformApplication.class, args);
+	}
+
+}
---
+++ Kids-IT-Learning-Platform/src/test/java/com/rajae_sekkat/Kids_IT_Learning_Platform/KidsItLearningPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajae_sekkat.Kids_IT_Learning_Platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidsItLearningPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
